5b20477d79577e57ee2f31ffa4c6f5e171f5d7d3
Blake Hamm <blake.j.hamm@gmail.com>
2024-10-19 10:25:51 -0600

    Setup opnsense vm on aorus proxmox node with lacp and bridges

--- tofu/main.tf
+++ tofu/main.tf
@@ -7,6 +7,14 @@ output "example_secret_output" {
   sensitive = true
 }
 
+resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
+  content_type = "iso"
+  datastore_id = "local"
+  node_name    = "aorus"
+
+  url = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
+}
+
 resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
   name      = "test-ubuntu"
   node_name = "aorus"
@@ -36,41 +44,45 @@ resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
   }
 
   network_device {
+    model  = "virtio"
     bridge = "vmbr0"
   }
 }
 
-resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
-  content_type = "iso"
-  datastore_id = "local"
-  node_name    = "aorus"
+resource "proxmox_virtual_environment_vm" "opnsense" {
+  name       = "opnsense"
+  node_name  = "aorus"
+  started    = true
+  boot_order = ["scsi0"]
 
-  url = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
+  cpu {
+    cores = 2
+    type  = "x86-64-v2-AES"
+  }
+
+  memory {
+    dedicated = 6144
+  }
+
+  disk {
+    datastore_id = "local"
+    interface    = "scsi0"
+    size         = 15
+  }
+  # cdrom {
+  #   enabled = true
+  #   file_id = "local:iso/OPNsense-24.7-dvd-amd64.iso"
+  #   interface="ide0"
+  # }
+
+  network_device {
+    model  = "virtio"
+    bridge = "vmbr1"
+  }
+
+  network_device {
+    model  = "virtio"
+    bridge = "vmbr2"
+  }
+
 }
-
-# resource "proxmox_vm_qemu" "opnsense" {
-#   name        = "opnsense"
-#   agent       = 0
-#   pxe         = true
-#   target_node = "aorus"
-#   vm_state    = "started"
-#   boot        = "order=scsi1,scsi0"
-#   network {
-#     bridge    = "vmbr0"
-#     firewall  = false
-#     link_down = false
-#     model     = "virtio"
-#   }
-#   disk {
-#     slot = "scsi0"
-#     size    = "10G"
-#     storage = "local"
-#     type    = "disk"
-#   }
-#   disk {
-#     slot = "scsi1"
-#     storage = "local"
-#     type    = "cdrom"
-#     iso = "iso/OPNsense-24.7-dvd-amd64.iso"
-#   }
-# }
