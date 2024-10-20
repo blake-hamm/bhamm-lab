25534cebc07d55ee4c8441e71bda8998ac7cf4c5
Blake Hamm <blake.j.hamm@gmail.com>
2024-10-20 09:02:44 -0600

    Update nfs mount path on nix and kubernetes

--- nix/modules/hardware/nfs-client.nix
+++ nix/modules/hardware/nfs-client.nix
@@ -1,7 +1,7 @@
 {
   networking.firewall.allowedTCPPorts = [ 2049 ];
-  fileSystems."/mnt/zpool_ssd" = {
-    device = "192.168.69.12:/zpool_ssd";
+  fileSystems."/mnt/storage" = {
+    device = "192.168.69.12:/mnt/storage";
     fsType = "nfs";
     options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" ];
   };
