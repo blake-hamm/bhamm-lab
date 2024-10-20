27952909a2ac588fd9dcd9ebeac71827326146ce
Blake Hamm <blake.j.hamm@gmail.com>
2024-10-20 09:50:46 -0600

    Ensure nfs export for different services is organized by directory

--- nix/modules/hardware/nfs-client.nix
+++ nix/modules/hardware/nfs-client.nix
@@ -1,7 +1,7 @@
 {
   networking.firewall.allowedTCPPorts = [ 2049 ];
-  fileSystems."/mnt/storage" = {
-    device = "192.168.69.12:/mnt/storage";
+  fileSystems."/mnt/storage/shared" = {
+    device = "192.168.69.12:/mnt/storage/shared";
     fsType = "nfs";
     options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" ];
   };
