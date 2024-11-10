a5dc3201a0375817806c3dbf3a4d7a41b855e767
Blake Hamm <blake.j.hamm@gmail.com>
2024-11-10 08:20:30 -0700

    Get initial working version of devshell

---
+++ nix/shell.nix
@@ -0,0 +1,49 @@
+{ pkgs }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    pre-commit
+    vault
+    kubectl
+    kubernetes-helm
+    argocd
+    k9s
+    colmena
+    poetry
+    sops
+    mkdocs
+    ansible
+    ansible-lint
+    sshpass
+    opentofu
+    terraform
+    tflint
+    trivy
+    terrascan
+  ];
+
+  packages = [
+    (pkgs.python3.withPackages (python-pkgs: [
+      python-pkgs.mkdocs-material
+      python-pkgs.hvac
+      python-pkgs.httpx
+      python-pkgs.jmespath
+    ]))
+  ];
+
+  shellHook = ''
+    # Install ansible galaxy requirements
+    ansible-galaxy install -r ansible/requirements.yml
+
+    # Source .env file if it exists
+    if [ -f .env ]; then
+      export $(grep -v '^#' .env | xargs)
+    fi
+
+    # Define python interpreter for ansible
+    export NIX_PYTHON_INTERPRETER=$(which python)
+
+    # Install pre commit
+    pre-commit install
+  '';
+}
