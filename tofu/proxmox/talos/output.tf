c7cc2fdda851c31be973d1ed424a096cf2d073c8
Blake Hamm <blake.j.hamm@gmail.com>
2025-04-10 11:56:33 -0600

    Ensure I can connect on local to cluster

--- tofu/proxmox/talos/output.tf
+++ tofu/proxmox/talos/output.tf
@@ -12,3 +12,15 @@ output "machine_config" {
   value     = data.talos_machine_configuration.this
   sensitive = true
 }
+
+resource "local_file" "talos_config" {
+  content         = data.talos_client_configuration.this.talos_config
+  filename        = "result/talos-config.yaml"
+  file_permission = "0600"
+}
+
+resource "local_file" "kube_config" {
+  content         = resource.talos_cluster_kubeconfig.this.kubeconfig_raw
+  filename        = "result/kube-config.yaml"
+  file_permission = "0600"
+}
