1a03e34655386a015f1a9c207488962c8a911f3d
Blake Hamm <blake.j.hamm@gmail.com>
2025-10-10 16:37:54 -0600

    Add cloudflare terraform

---
+++ tofu/cloudflare/providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.7" # For open tofu
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 5"
+    }
+  }
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
---
+++ tofu/cloudflare/variables.tf
@@ -0,0 +1,28 @@
+variable "cloudflare_api_token" {
+  description = "API token for cloudflare auth."
+  type        = string
+}
+
+
+variable "cloudflare_account_id" {
+  description = "Cloudflare account ID."
+  type        = string
+}
+
+variable "truenas_bucket_name" {
+  description = "The name of the R2 bucket for TrueNAS."
+  type        = string
+  default     = "truenas-backups"
+}
+
+variable "truenas_bucket_location" {
+  description = "The location of the R2 bucket for TrueNAS."
+  type        = string
+  default     = "wnam"
+}
+
+variable "truenas_bucket_storage_class" {
+  description = "The storage class of the R2 bucket for TrueNAS."
+  type        = string
+  default     = "Standard"
+}
