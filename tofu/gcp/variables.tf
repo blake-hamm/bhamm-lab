bfbe5bad3f41c490b453bfe0eb5c07e9fff6f77d
Blake Hamm <19966313+blake-hamm@users.noreply.github.com>
2025-10-12 17:11:57 -0600

    Release/expand ceph (#50)
    * Adjust swfs bootstrap s3 credentials
    
    * Rename swfs backup bucket
    
    * Update ceph rbd permissions
    
    * Update automations branch
    
    * Update automations branch
    
    * Add additional r2 credentials and minio endpoint
    
    * Update bucket name
    
    * Update swfs secrets
    
    * Update swfs secrets
    
    * Add swfs step to sync to cloudflare r2
    
    * Refine rclone sync flags
    
    * Add rclone to local nix shel
    
    * Optimize rclone sync command
    
    * Further optimize swfs rclone sync
    
    * Add checksum command
    
    * Adjust rclone sync command
    
    * Fix rclone sync error
    
    * Add pbs
    
    * Ensure swfs stable secret
    
    * Add seaweedfs_s3_config to swfs external secret
    
    * Ensure seaweeedfs uses existing secret for s3 auth
    
    * Add additional swfs creds
    
    * Ensure init secret has proper swfs creds
    
    * Remove swfs push creds
    
    * Update branch name for core and apps
    
    * Adjust target restore for swfs
    
    * Adjust memory multiplier
    
    * Update green swfs restore target from gcs to minio
    
    * Update ceph cleanup script
    
    * Remove gcs storage resources
    
    * Move docs site and add dashy links
    
    * Adjust branch to main
    
    * Fix dashy names

--- tofu/gcp/variables.tf
+++ tofu/gcp/variables.tf
@@ -10,25 +10,8 @@ variable "region" {
   default     = "us-central1"
 }
 
-variable "k8up_service_account_id" {
-  description = "The ID for the k8up service account"
-  type        = string
-  default     = "k8up-backups"
-}
-
 variable "sops_key_file_path" {
   description = "Path to save the generated key file."
   type        = string
   default     = "./result/gcp-sops-sa.json"
-}
-
-variable "k8up_key_file_path" {
-  description = "Path to save the generated key file."
-  type        = string
-  default     = "./result/gcp-k8up-sa.json"
-}
-variable "k8up_hmac_credentials_path" {
-  description = "Path to save the generated key file."
-  type        = string
-  default     = "./result/gcp-k8up-hmac.json"
 }
