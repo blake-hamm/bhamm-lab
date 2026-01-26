13a645aaf9759a6580254545a414f7eacc24595e
Blake Hamm <blake.j.hamm@gmail.com>
2026-01-26 14:08:42 -0700

    Bump up nix version and flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nix config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     disko = {
@@ -11,11 +11,11 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    catppuccin.url = "github:catppuccin/nix/release-25.05";
+    catppuccin.url = "github:catppuccin/nix/release-25.11";
 
     sops-nix.url = "github:Mic92/sops-nix";
 
