7901f61935df24aa978a7236d245f9715dd3de59
Blake Hamm <blake.j.hamm@gmail.com>
2026-02-04 20:46:48 -0700

    Troubleshoot nix laptop

--- nix/hosts/framework/hardware-configuration.nix
+++ nix/hosts/framework/hardware-configuration.nix
@@ -13,7 +13,7 @@
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
-  boot.kernelParams = [ "tsc=unstable" ];
+  boot.kernelParams = [ "tsc=unstable" "amdgpu.dcdebugmask=0x10" ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
