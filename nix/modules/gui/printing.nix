e12e506a9f9a608efddafa19c6e9834dd4671580
Blake Hamm <blake.j.hamm@gmail.com>
2026-04-12 21:09:21 -0600

    Fix printing for orange pi

--- nix/modules/gui/printing.nix
+++ nix/modules/gui/printing.nix
@@ -1,14 +1,17 @@
-{ pkgs, ... }:
+{ lib, config, pkgs, ... }:
 {
+  config = lib.mkIf config.cfg.gnome.enable {
 
-  services.printing = {
-    enable = true;
-    drivers = [ pkgs.brlaser pkgs.brgenml1lpr pkgs.brgenml1cupswrapper ];
-  };
+    services.printing = {
+      enable = true;
+      drivers = [ pkgs.brlaser pkgs.brgenml1lpr pkgs.brgenml1cupswrapper ];
+    };
 
-  services.avahi = {
-    enable = true;
-    nssmdns4 = true;
-    openFirewall = true;
+    services.avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
+
   };
 }
