1aa99e84a18dd3e16008c666605a8afbc0650b90
Blake Hamm <blake.j.hamm@gmail.com>
2026-05-03 15:40:52 -0600

    Configure zsh

--- nix/modules/core/zsh/default.nix
+++ nix/modules/core/zsh/default.nix
@@ -25,14 +25,54 @@
         autosuggestion.enable = true;
         syntaxHighlighting.enable = true;
 
+        history = {
+          size = 100000;
+          save = 100000;
+          ignoreDups = true;
+          ignoreSpace = true;
+          share = true;
+        };
+
         shellAliases = {
           ll = "ls -al";
         };
 
-        initContent = ''
-          source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
-          source ~/.config/p10k.zsh
-        '';
+        initContent = lib.mkMerge [
+          (lib.mkBefore ''
+            if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+              source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+            fi
+          '')
+          ''
+            setopt appendhistory
+            setopt incappendhistory
+            setopt histreduceblanks
+            setopt histverify
+            setopt no_nomatch
+            setopt interactivecomments
+
+            zstyle ':completion:*' use-cache on
+            zstyle ':completion:*' cache-path ~/.zsh/cache
+            zstyle ':completion:*' menu select
+            zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+            zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"
+            zstyle ':completion:*:*:*:*:descriptions' format '%F{green}-- %d --%f'
+            zstyle ':completion:*' group-name ""
+
+            source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+            source ~/.config/p10k.zsh
+          ''
+        ];
+      };
+
+      programs.fzf = {
+        enable = true;
+        enableZshIntegration = true;
+      };
+
+      programs.direnv = {
+        enable = true;
+        nix-direnv.enable = true;
       };
 
       xdg.configFile."p10k.zsh".source = ./p10k.zsh;
